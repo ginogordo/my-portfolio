31b9f8024b871c36076381525381d3b618a70888
Gino <ginogordo123@gmail.com>
2024-08-25 05:45:29 -0500

    unfinished

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-document.getElementById('menu-btn').addEventListener('click', function() {
-    var navLinks = document.getElementById('navLinks');
-    navLinks.classList.toggle('active');
+document.getElementById("menu-btn").addEventListener("click", function () {
+  var navLinks = document.getElementById("navLinks");
+  navLinks.classList.toggle("active");
+});
+
+var navLinks = document.querySelectorAll(".nav-links a");
+navLinks.forEach(function (link) {
+  link.addEventListener("click", function () {
+    var navLinksContainer = document.getElementById("navLinks");
+    navLinksContainer.classList.remove("active");
+  });
 });
